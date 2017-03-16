dbd81b50402592e91f78f25532f2f1aac809d2bd
unknown <782258734@qq.com>
2017-03-16 21:25:25 +0800

    Firsted

---
+++ coin/src/coin/Coin.java
@@ -0,0 +1,35 @@
+package coin;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Coin {
+	private HashMap<Integer, String> coinnames = new HashMap<Integer,String>();
+	public Coin(){
+		coinnames.put(1, "penny");
+		coinnames.put(10, "dime");
+		coinnames.put(25, "quarter");
+		coinnames.put(50, "half-dollar");
+		coinnames.put(50, "ÎåÃ«");
+		System.out.println(coinnames.keySet().size());
+		for( int k : coinnames.keySet() ){
+			System.out.println(coinnames.get(k));
+		}
+		System.out.println(coinnames);
+	}
+	public String get(int amount){
+		if( coinnames.containsKey(amount) )
+			return coinnames.get(amount);
+		else
+			return "NOT FOUND";
+	}
+	public static void main(String[] args) {
+		Coin coin = new Coin();
+		Scanner in = new Scanner(System.in);
+		int amount = in.nextInt();
+		System.out.println(coin.get(amount));
+		
+
+	}
+
+}
